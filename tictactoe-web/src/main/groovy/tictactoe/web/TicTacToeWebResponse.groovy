795be241b19ac004d0a9dfef09fcadc5dedbac10
Gerd Aschemann <gerd@aschemann.net>
2013-03-18 22:29:13 +0100

    - Added Exception handling - Improved help message - Added plausibility check for occupied field

--- tictactoe-web/src/main/groovy/tictactoe/web/TicTacToeWebResponse.groovy
+++ tictactoe-web/src/main/groovy/tictactoe/web/TicTacToeWebResponse.groovy
@@ -1,9 +1,13 @@
 package tictactoe.web
 
+import org.slf4j.LoggerFactory
+
 class TicTacToeWebResponse {
 	enum RESPONSE_FORMAT {
 		RAW, HTML
 	}
+	
+	static def logger = LoggerFactory.getLogger("tictactoe.web.TicTacToeWebResponse")
 
 	def out = null
 	def responseFormat = RESPONSE_FORMAT.RAW
@@ -11,8 +15,11 @@ class TicTacToeWebResponse {
 
 	TicTacToeWebResponse(out, response, params = null) {
 		this.out = out
-		if (params && params['TYPE'] && params['TYPE'] == "HTML") {
+		if (params && params['FORMAT'] && params['FORMAT'] == "HTML") {
 			this.responseFormat = RESPONSE_FORMAT.HTML
+			logger.debug("HTML format chosen")
+		} else {
+			logger.debug("RAW format chosen")
 		}
 		this.response = response
 	}
@@ -37,22 +44,32 @@ ${msg}
 		}
 
 	}
-
-	void create () {
-		respond ("NEW")
+	
+	void preFormat (msg) {
+		if (responseFormat == RESPONSE_FORMAT.HTML) {
+			respond """<pre>
+$msg
+</pre>
+"""
+		} else {
+			respond msg
+		}
 	}
 
 	void help () {
-		respond """<pre>
-CMD=DESTROY							-	Invalidate Session
-CMD=RESET                           -	Reset board
+		preFormat """CMD=DESTROY                         -	Invalidate Session
+CMD=HELP                            -	Show this help text
 CMD=SET&coordinate=c&value=[xo]	    -	Set 'value' ('x' or 'o') to field with coordinates [A-C][1-3] 
 CMD=SHOW                            -	Show board
-</pre>"""
+CMD=RESET                           -	Reset board"""
 	}
 
 	void error (msg) {
-		respond msg
+		respond "ERROR: $msg"
+	}
+
+	void exception (msg) {
+		respond "EXCEPTION: $msg"
 	}
 
 	void show(String status) {
--- tictactoe-web/src/main/webapp/simple.groovy
+++ tictactoe-web/src/main/webapp/simple.groovy
@@ -5,52 +5,69 @@ import tictactoe.core.Coordinate
 import tictactoe.core.Value
 import tictactoe.web.TicTacToeWebResponse
 
+
 def logger = LoggerFactory.getLogger("tictactoe.web.simple")
 
 webResponse = new TicTacToeWebResponse(out, response, params)
 
-if (!session) {
-	logger.info("New session from IP {}", request.remoteHost)
-	session = request.getSession(true)
-}
+try {
+	if (!session) {
+		logger.debug("New session from IP '{}'", request.remoteHost)
+		session = request.getSession(true)
+	} else {
+		logger.debug("Old session from IP '{}'", request.remoteHost)
+	}
 
-def board = session.getAt("Board")
+	def board = session.getAt("Board")
 
-if (null == board) {
-	webResponse.create()
-	board = new Board()
-	logger.debug("Add new board to session: '${board}'")
-	session.putAt("Board",	board)
-	
-	return
-} else {
-	logger.debug("Retrieved Board: '${board}'")
-}
+	if (null == board) {
+		board = new Board()
+		logger.debug("Add new board to session: '${board}'")
+		session.putAt("Board",	board)
+	} else {
+		logger.debug("Retrieved Board: '${board}'")
+	}
 
-if (params.isEmpty()) {
-	webResponse.help()
-	return
-}
+	if (params.isEmpty()) {
+		webResponse.help()
+		return
+	}
 
-cmd =  params['CMD']
+	cmd =  params['CMD']
 
-if (!cmd) {
-	webResponse.error("No CMD parameter given")
-} else if (cmd == "HELP") {
-	webResponse.help()
-} else if (cmd == "SHOW") {
-	webResponse.show(board.toString())
-} else if (cmd == "RESET") {
-	board.reset()
-	webResponse.ok()
-} else if (cmd == "DESTROY") {
-	session.invalidate()
-	webResponse.destroyed()
-} else if (cmd == "SET") {
-	Coordinate coordinate = Coordinate.valueOf(params['coordinate'])
-	Value value = Value.valueOf(params['value'])
-	board.setValue(coordinate, value)
-	webResponse.show(board.toString())
-} else {
-	webResponse.error("Unknown command '${cmd}'")
+	if (!cmd || cmd == "SHOW") {
+		webResponse.show(board.toString())
+	} else if (cmd == "HELP") {
+		webResponse.help()
+	} else if (cmd == "RESET") {
+		board.reset()
+		webResponse.ok()
+	} else if (cmd == "DESTROY") {
+		session.invalidate()
+		webResponse.destroyed()
+	} else if (cmd == "SET") {
+		def coordinateValue = params['coordinate']
+		if (!coordinateValue) {
+			webResponse.error("Parameter 'coordinate' is missing")
+			return
+		}
+		Coordinate coordinate = Coordinate.valueOf(coordinateValue)
+		if (board.isSet(coordinate)) {
+			webResponse.error("Field at ${coordinate} is already occupied")
+			return
+		}
+		def valueValue = params['value']
+		if (!coordinateValue) {
+			webResponse.error("Parameter 'value' is missing")
+			return
+		}
+		Value value = Value.valueOf(valueValue)
+		board.setValue(coordinate, value)
+		webResponse.show(board.toString())
+	} else {
+		webResponse.error("Unknown command '${cmd}'")
+	}
+} catch (Exception error) {
+	logger.error("Error: {}", error.message)
+	webResponse.exception("'${error.message}'")
 }
